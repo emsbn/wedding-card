3c1729588c0595c1897b33c82232caad00d36ac5
emsbn <emsbn1@gmail.com>
2021-08-04 17:14:25 +0900

    fix: Fix image path

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -43,31 +43,31 @@ const Story = ({ zValue }) => {
       <Frame3D ref={frame3DRef} className="Frame3D">
         <Frame ref={frameRefs[0]}>
           <PhotoContainer>
-            <Photo src="/images/story/test.jpg" alt="test" />
+            <Photo src="images/story/test.jpg" alt="test" />
             <Text>9월의 어느 아름다운 날</Text>
           </PhotoContainer>
         </Frame>
         <Frame ref={frameRefs[1]}>
           <PhotoContainer>
-            <Photo src="/images/story/test_1.jpg" alt="test" />
+            <Photo src="images/story/test_1.jpg" alt="test" />
             <Text>살아온 환경, 좋아하는 것도 다른 저희가</Text>
           </PhotoContainer>
         </Frame>
         <Frame ref={frameRefs[2]}>
           <PhotoContainer>
-            <Photo src="/images/story/test_2.png" alt="test" />
+            <Photo src="images/story/test_2.png" alt="test" />
             <Text>이제 서로를 닮아 가려 합니다.</Text>
           </PhotoContainer>
         </Frame>
         <Frame ref={frameRefs[3]}>
           <PhotoContainer>
-            <Photo src="/images/story/test_3.jpg" alt="test" />
+            <Photo src="images/story/test_3.jpg" alt="test" />
             <Text>혼자가 아닌 우리로서 행복하게 잘 살겠습니다.</Text>
           </PhotoContainer>
         </Frame>
         <Frame ref={frameRefs[4]}>
           <PhotoContainer>
-            <Photo src="/images/story/test_4.jpg" alt="test" />
+            <Photo src="images/story/test_4.jpg" alt="test" />
             <Text>9월 소중한 약속의 날을 축복해주십시오.</Text>
           </PhotoContainer>
         </Frame>
