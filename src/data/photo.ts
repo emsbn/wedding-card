dbfecede2f493e55fff21fe7ab917231b18b6026
emsbn <emsbn1@gmail.com>
2021-08-10 19:53:06 +0900

    feat: Add photo

--- src/data/photo.ts
+++ src/data/photo.ts
@@ -37,4 +37,52 @@ export const photoData: iPhoto[] = [
     path: './images/photo/wedding-bg-9.jpg',
     title: 'bride and groom 9',
   },
+  {
+    path: './images/photo/wedding-b-1.jpg',
+    title: 'bride 1',
+  },
+  {
+    path: './images/photo/wedding-b-2.jpg',
+    title: 'bride 2',
+  },
+  {
+    path: './images/photo/wedding-b-3.jpg',
+    title: 'bride 3',
+  },
+  {
+    path: './images/photo/wedding-b-4.jpg',
+    title: 'bride 4',
+  },
+  {
+    path: './images/photo/wedding-b-5.jpg',
+    title: 'bride 5',
+  },
+  {
+    path: './images/photo/wedding-b-6.jpg',
+    title: 'bride 6',
+  },
+  {
+    path: './images/photo/wedding-b-7.jpg',
+    title: 'bride 7',
+  },
+  {
+    path: './images/photo/wedding-b-8.jpg',
+    title: 'bride 8',
+  },
+  {
+    path: './images/photo/wedding-b-9.jpg',
+    title: 'bride 9',
+  },
+  {
+    path: './images/photo/wedding-b-10.jpg',
+    title: 'bride 10',
+  },
+  {
+    path: './images/photo/wedding-b-11.jpg',
+    title: 'bride 11',
+  },
+  {
+    path: './images/photo/wedding-g-1.jpg',
+    title: 'groom 1',
+  },
 ];
