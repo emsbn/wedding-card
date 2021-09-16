8aa19e022ae012105b1706a84cb19cdace5a945f
emsbn <emsbn1@gmail.com>
2021-09-16 13:02:47 +0900

    feat: Add secret mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import ReactGA from 'react-ga';
-import { Typography } from '@material-ui/core';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
 
 import './App.css';
 import HeaderContainer from './layouts/Header';
 import BodyContainer from './layouts/Body';
 import FooterContainer from './layouts/Footer';
 
+const secret = '1719';
+
 function App() {
   const [zValue, setZValue] = useState<number>(0);
   const [maxScroll, setMaxScroll] = useState<number>(0);
+  const [secretInput, setSecretInput] = useState<string>('');
+  const [show, setShow] = useState<boolean>(false);
   const scrollbarRef = useRef<Scrollbars>(null);
   const announceRef = useRef<HTMLDivElement>(null);
   const contactRef = useRef<typeof Typography>(null);
+
+  const onChangeSecretInput = useCallback(e => {
+    setSecretInput(e.target.value);
+  }, []);
+
+  const onSubmitSecret = useCallback(() => {
+    if (secretInput === secret) setShow(true);
+  }, [secretInput]);
 
   useEffect(() => {
     ReactGA.initialize('UA-151723707-1');
@@ -36,7 +48,7 @@ function App() {
     [maxScroll, scrollbarRef, zValue],
   );
 
-  return (
+  return show ? (
     <Scrollbars ref={scrollbarRef} onScrollFrame={onScroll} style={{ height: '100vh' }}>
       <div className="App">
         <HeaderContainer scrollbarRef={scrollbarRef} announceRef={announceRef} contactRef={contactRef} />
@@ -44,6 +56,19 @@ function App() {
         <FooterContainer />
       </div>
     </Scrollbars>
+  ) : (
+    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
+      <Box display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start" my={2}>
+        <Typography>채은과 수빈의 청첩장 입니다.</Typography>
+        <Typography>비밀번호를 입력하시면 보실 수 있습니다.</Typography>
+      </Box>
+      <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" my={2}>
+        <TextField label="Secret" variant="outlined" onChange={onChangeSecretInput} style={{ marginRight: '1rem' }} />
+        <Button variant="contained" color="primary" onClick={onSubmitSecret} size={'large'}>
+          제출
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
